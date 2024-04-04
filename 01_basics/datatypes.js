1c1358369f0f9d4c7078e8146a086685abf82833
Neha Rajpoot <132067615+neharakesh@users.noreply.github.com>
2024-04-04 05:13:36 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+//alert (3+3) we are using node js not browser 
+let name="neha";
+let age=23;
+let isLoggedIn=false;
+//number=>2^53;
+//bigint
+//bollean=>true/false
+//string=""
+//undefined=>value is not assigned yet
+//null=>standalone value (object)
+//symbol=>unique
+
+
+//object
+
+console.log(typeof(age));
---
+++ 01_basics/var.js
@@ -0,0 +1,7 @@
+const accountId=1457657;
+let accountEmail="neha@234gmail.com";
+var accountPassword="1234";
+/*dont use var it is very dangerous to because of blockissue and functional use*/
+accountCity="jaopur";
+console.log(accountCity);
+console.table([accountId,accountEmail])
