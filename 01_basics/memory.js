afbadecda91bfc46342b453bd62d685475245ebc
Neha Rajpoot <132067615+neharakesh@users.noreply.github.com>
2024-04-05 13:39:27 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,15 @@
+// stack (primitive),Heap (non -primitive)
+let myYoutubeName="neharajpoot";
+let anotherName=myYoutubeName;
+anotherName="chaiaurcode";
+console.log(myYoutubeName);
+console.log(anotherName);
+let user={
+    email:"neharajpoot222@gmail.com",
+    name:"neha"
+
+}
+let user2=user
+user2.email="haa@gmail.com";
+console.log(user.email);
+console.log(user2.email);
---
+++ 01_basics/operations.js
@@ -0,0 +1,8 @@
+ let str1="neha"
+ let str2="rajpoot"
+ let str=str1+str2;
+ console.log(str)
+ console.log("1"+2+2)
+ console.log(1+2+"3")
+ console.log(+true)
+ console.log(+1)
